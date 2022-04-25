2362396cbb2561f4770a9b284eed497f51e72bb8
anirban-1009 <anirban9293@gmail.com>
2022-04-25 20:30:27 +0530

    adding mask folder

--- face_det.py
+++ face_det.py
@@ -8,7 +8,7 @@ cap = cv2.VideoCapture(0)
 while 1 :
 	ret, img = cap.read()#getting the frame data
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#turning image into a grayscale image
-	faces = face_cascade.detectMultiScale(gray, ScaleFactor=1.3, minNeighbors=5)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	#getting the co-ordinates of detected face with the height and width
 	#ScaleFactor determines the factor of increase in window size
 	# Higher the values of the “minNeighbors”, less will be the number of false positives, and less error will be in terms of false detection of faces
